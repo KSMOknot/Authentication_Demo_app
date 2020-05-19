a2d23ce9f7b5121c3e9d36d3be10b5eb511e3a92
Miguel Benevento <galakseas@gmail.com>
2020-05-19 19:50:11 -0400

    Created Auth route/Login Routes

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -53,6 +54,18 @@ app.post('/register', function(req, res) {
                 res.redirect("/secret");
         });
     });
+});
+
+// LogIn Routes
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+// login logic
+app.post('/login', passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
 });
 
 app.listen( port = 8000, function(){
