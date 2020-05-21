f472b7cd5fcf79fc73beea4a491775ae9b1a7255
Miguel Benevento <galakseas@gmail.com>
2020-05-21 19:51:00 -0400

    Added login/logout buttons & middleware

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
-app.get('/secret', function(req, res) {
+app.get('/secret', isLoggedIn, function(req, res) {
     res.render('secret');
 });
 
@@ -62,11 +62,24 @@ app.get('/login', function(req, res){
 });
 
 // login logic
+// middleware
 app.post('/login', passport.authenticate("local", {
     successRedirect: "/secret",
     failureRedirect: "/login"
 }), function(req, res){
 });
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen( port = 8000, function(){
     console.log("App has started");
